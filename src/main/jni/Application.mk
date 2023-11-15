2d0b9f6ae297a7d361ddfca3ed21fa12cbb979e8
Vincent <vincent@practical-mais-b43.int.infomaniak.ch>
2023-11-15 13:42:52 +0100

    Change APP_ABI to all to cover all possible architectures

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,7 +4,4 @@ APP_CPPFLAGS += -fexceptions
 #For ANativeWindow support
 APP_PLATFORM = android-21
 
-APP_ABI :=  armeabi-v7a \
-            arm64-v8a \
-            x86 \
-            x86_64
+APP_ABI :=  all
