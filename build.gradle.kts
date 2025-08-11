fb3dbe12337b59ce53528b477ddd2ca2b755cdb1
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-08-11 11:09:26 +0200

    chore: Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         set("libTargetSdk", 34)
 
         set("libGroupId", "com.github.infomaniak")
-        set("libVersionName", "1.9.7")
+        set("libVersionName", "1.9.8")
         set("libArtifactId", "pdfiumandroid")
         set("ndkVersion", "28.2.13676358")
         set("javaVersion", JavaVersion.VERSION_17)
