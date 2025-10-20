0ab6bf59f0b7c068efbff15ca686257aceb300ef
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-10-20 14:56:27 +0200

    chore: Update licence

--- src/main/jni/include/fpdf_save.h
+++ src/main/jni/include/fpdf_save.h
@@ -1,9 +1,21 @@
-// Copyright 2014 The PDFium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
-
+/*
+ * Infomaniak PDFium - Android
+ * Copyright (C) 2014 Foxit Software Inc. http://www.foxitsoftware.com
+ * Copyright (C) 2014 The PDFium Authors
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef PUBLIC_FPDF_SAVE_H_
 #define PUBLIC_FPDF_SAVE_H_
 
