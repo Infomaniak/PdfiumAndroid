c0c3b5fa53f588607785d8aebf932e91b566598f
Vincent Te <vincent.te@infomaniak.com>
2026-07-22 14:37:29 +0200

    chore: Set target and compile SDK to 37

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ buildscript {
 
     extra.apply {
         set("libMinSdk", 21)
-        set("libCompileSdk", 36)
-        set("libTargetSdk", 36)
+        set("libCompileSdk", 37)
+        set("libTargetSdk", 37)
 
         set("libGroupId", "com.github.infomaniak")
         set("libVersionName", "1.9.11")
